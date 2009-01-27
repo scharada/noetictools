e6459254ea0af36b70de6fcaa00707f85ce0ec2c
Wobbit42 <Wobbit42@50de55f2-9c48-0410-bb6b-63ee2139710e>
2009-01-27 07:22:50 +0000

    Code clean-up

--- trunk/NoeticTools/PlugInService/Menus/ToolStripMenuService.cs
+++ trunk/NoeticTools/PlugInService/Menus/ToolStripMenuService.cs
@@ -1,100 +1,98 @@
 #region Copyright
 
-/*---------------------------------------------------------------------------
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- * 
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations under 
- * the License.
- * 
- * The Initial Developer of the Original Code is Robert Smyth.
- * Portions created by Robert Smyth are Copyright (C) 2008.
- * 
- * All Rights Reserved.
- *---------------------------------------------------------------------------*/
+// The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in compliance
+//  with the License. You may obtain a copy of the License at
+//  
+//  http://www.mozilla.org/MPL/
+//  
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//  License for the specific language governing rights and limitations under 
+//  the License.
+//  
+//  The Initial Developer of the Original Code is Robert Smyth.
+//  Portions created by Robert Smyth are Copyright (C) 2008.
+//  
+//  All Rights Reserved.
 
-#endregion //Copyright
+#endregion
 
 using NoeticTools.DotNetWrappers;
+using ToolStripMenuItem=System.Windows.Forms.ToolStripMenuItem;
 
 
 namespace NoeticTools.PlugIns.Menus
 {
-	public class ToolStripMenuService : IToolStripMenuService
-	{
-		private readonly IMenuStrip menuStrip;
+    public class ToolStripMenuService : IToolStripMenuService
+    {
+        private readonly IMenuStrip menuStrip;
 
-		public ToolStripMenuService(IMenuStrip menuStrip)
-		{
-			this.menuStrip = menuStrip;
-		}
+        public ToolStripMenuService(IMenuStrip menuStrip)
+        {
+            this.menuStrip = menuStrip;
+        }
 
-		public IToolStripMenuItem AddMenuItem(string menuPath)
-		{
-			string[] menuItemNames = menuPath == string.Empty ? new string[0] : menuPath.Split('|');
+        public IToolStripMenuItem AddMenuItem(string menuPath)
+        {
+            string[] menuItemNames = menuPath == string.Empty ? new string[0] : menuPath.Split('|');
 
-			IToolStripMenuItem menuItem = null;
-			foreach (string newItemText in menuItemNames)
-			{
-				string menuName = GetMenuNameFromText(newItemText);
+            IToolStripMenuItem menuItem = null;
+            foreach (string newItemText in menuItemNames)
+            {
+                string menuName = GetMenuNameFromText(newItemText);
 
-				if (menuStrip.Items.ContainsKey(menuName))
-				{
-					menuItem = (IToolStripMenuItem)menuStrip.Items[menuName];
-				}
-				else
-				{
-					menuItem = AddMenuItem(menuItem, newItemText);
-				}
+                if (menuStrip.Items.ContainsKey(menuName))
+                {
+                    menuItem = (IToolStripMenuItem) menuStrip.Items[menuName];
+                }
+                else
+                {
+                    menuItem = AddMenuItem(menuItem, newItemText);
+                }
+            }
 
-			}
+            return menuItem;
+        }
 
-			return menuItem;
-		}
+        private IToolStripMenuItem AddMenuItem(IToolStripDropDownItem parentMenuItem, string newItemText)
+        {
+            IToolStripMenuItem newMenuItem = new DotNetWrappers.ToolStripMenuItem(new ToolStripMenuItem());
+            newMenuItem.Text = newItemText;
+            newMenuItem.Name = GetMenuNameFromText(newItemText);
 
-		private IToolStripMenuItem AddMenuItem(IToolStripDropDownItem parentMenuItem, string newItemText)
-		{
-			IToolStripMenuItem newMenuItem = new ToolStripMenuItem(new System.Windows.Forms.ToolStripMenuItem());
-			newMenuItem.Text = newItemText;
-			newMenuItem.Name = GetMenuNameFromText(newItemText);
+            if (parentMenuItem == null)
+            {
+                AddTopLevelMenu(newMenuItem);
+            }
+            else
+            {
+                parentMenuItem.DropDownItems.Add(newMenuItem);
+            }
 
-			if (parentMenuItem == null)
-			{
-				AddTopLevelMenu(newMenuItem);
-			}
-			else
-			{
-				parentMenuItem.DropDownItems.Add(newMenuItem);
-			}
+            return newMenuItem;
+        }
 
-			return newMenuItem;
-		}
+        private static string GetMenuNameFromText(string menuText)
+        {
+            return menuText.Replace("&", string.Empty).ToLower();
+        }
 
-		private static string GetMenuNameFromText(string menuText)
-		{
-			return menuText.Replace("&", string.Empty).ToLower();
-		}
-
-		private void AddTopLevelMenu(IToolStripItem newMenuItem)
-		{
-			int insertIndex = menuStrip.Items.IndexOfKey("windowsMenu");
-			if (insertIndex == -1)
-			{
-				insertIndex = menuStrip.Items.IndexOfKey("Help");
-			}
-			if (insertIndex == -1)
-			{
-				menuStrip.Items.Add(newMenuItem);
-			}
-			else
-			{
-				menuStrip.Items.Insert(insertIndex, newMenuItem);
-			}
-		}
-	}
+        private void AddTopLevelMenu(IToolStripItem newMenuItem)
+        {
+            int insertIndex = menuStrip.Items.IndexOfKey("windowsMenu");
+            if (insertIndex == -1)
+            {
+                insertIndex = menuStrip.Items.IndexOfKey("Help");
+            }
+            if (insertIndex == -1)
+            {
+                menuStrip.Items.Add(newMenuItem);
+            }
+            else
+            {
+                menuStrip.Items.Insert(insertIndex, newMenuItem);
+            }
+        }
+    }
 }
--- trunk/NoeticTools/PlugInService/Menus/ToolStripMenuServicePlugIn.cs
+++ trunk/NoeticTools/PlugInService/Menus/ToolStripMenuServicePlugIn.cs
@@ -1,43 +1,41 @@
 #region Copyright
 
-/*---------------------------------------------------------------------------
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- * 
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations under 
- * the License.
- * 
- * The Initial Developer of the Original Code is Robert Smyth.
- * Portions created by Robert Smyth are Copyright (C) 2008.
- * 
- * All Rights Reserved.
- *---------------------------------------------------------------------------*/
+// The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in compliance
+//  with the License. You may obtain a copy of the License at
+//  
+//  http://www.mozilla.org/MPL/
+//  
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//  License for the specific language governing rights and limitations under 
+//  the License.
+//  
+//  The Initial Developer of the Original Code is Robert Smyth.
+//  Portions created by Robert Smyth are Copyright (C) 2008.
+//  
+//  All Rights Reserved.
 
-#endregion //Copyright
+#endregion
 
 using NoeticTools.DotNetWrappers;
 
 
 namespace NoeticTools.PlugIns.Menus
 {
-	public class ToolStripMenuServicePlugIn : IPlugin
-	{
-		private readonly IMenuStrip menuStrip;
+    public class ToolStripMenuServicePlugIn : IPlugin
+    {
+        private readonly IMenuStrip menuStrip;
 
-		public ToolStripMenuServicePlugIn(IMenuStrip menuStrip)
-		{
-			this.menuStrip = menuStrip;
-		}
+        public ToolStripMenuServicePlugIn(IMenuStrip menuStrip)
+        {
+            this.menuStrip = menuStrip;
+        }
 
-		void IPlugin.Accept(IPluginHostServices hostServices)
-		{
-			ToolStripMenuService service = new ToolStripMenuService(menuStrip);
-			hostServices.AddService<IToolStripMenuService>(service);
-		}
-	}
+        void IPlugin.Accept(IPluginHostServices hostServices)
+        {
+            ToolStripMenuService service = new ToolStripMenuService(menuStrip);
+            hostServices.AddService<IToolStripMenuService>(service);
+        }
+    }
 }
