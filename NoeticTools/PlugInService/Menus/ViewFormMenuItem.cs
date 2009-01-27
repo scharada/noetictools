08602238011bbcabd3c5a49e9f43b31084fc66c7
Wobbit42 <Wobbit42@50de55f2-9c48-0410-bb6b-63ee2139710e>
2009-01-27 07:22:50 +0000

    Code clean-up

--- NoeticTools/PlugInService/Menus/ViewFormMenuItem.cs
+++ NoeticTools/PlugInService/Menus/ViewFormMenuItem.cs
@@ -1,90 +1,87 @@
 #region Copyright
 
-/*---------------------------------------------------------------------------
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- * 
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations under 
- * the License.
- * 
- * The Initial Developer of the Original Code is Robert Smyth.
- * Portions created by Robert Smyth are Copyright (C) 2008.
- * 
- * All Rights Reserved.
- *---------------------------------------------------------------------------*/
+// The contents of this file are subject to the Mozilla Public License
+//  Version 1.1 (the "License"); you may not use this file except in compliance
+//  with the License. You may obtain a copy of the License at
+//  
+//  http://www.mozilla.org/MPL/
+//  
+//  Software distributed under the License is distributed on an "AS IS"
+//  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//  License for the specific language governing rights and limitations under 
+//  the License.
+//  
+//  The Initial Developer of the Original Code is Robert Smyth.
+//  Portions created by Robert Smyth are Copyright (C) 2008.
+//  
+//  All Rights Reserved.
 
 #endregion
 
 using System;
-using System.Windows.Forms;
 using NoeticTools.DotNetWrappers;
 
 
 namespace NoeticTools.PlugIns.Menus
 {
-	public class ViewFormMenuItem : IViewFormMenuItem, IFormClosedListener
-	{
-		private readonly IViewController viewController;
-		private readonly IToolStripMenuItem viewMenu;
-		private bool viewShown = false;
+    public class ViewFormMenuItem : IViewFormMenuItem, IFormClosedListener
+    {
+        private readonly IViewController viewController;
+        private readonly IToolStripMenuItem viewMenu;
+        private bool viewShown;
 
-		public ViewFormMenuItem(string menuItemText, IPluginHostServices hostServices, IViewController viewController)
-		{
-			this.viewController = viewController;
-			IToolStripMenuService menusService = hostServices.GetService<IToolStripMenuService>();
-			viewMenu = menusService.AddMenuItem("&View|" + menuItemText);
+        public ViewFormMenuItem(string menuItemText, IPluginHostServices hostServices, IViewController viewController)
+        {
+            this.viewController = viewController;
+            IToolStripMenuService menusService = hostServices.GetService<IToolStripMenuService>();
+            viewMenu = menusService.AddMenuItem("&View|" + menuItemText);
 
-			UpdateMenu();
+            UpdateMenu();
 
-			viewMenu.Click += viewMenu_Click;
-		}
+            viewMenu.Click += viewMenu_Click;
+        }
 
-		private void viewMenu_Click(object sender, EventArgs e)
-		{
-			ToggleViewShown();
-		}
-
-		public void ToggleViewShown()
-		{
-			if (viewShown)
-			{
-				CloseView();
-			}
-			else
-			{
-				ShowView();
-			}
-			UpdateMenu();
-		}
-
-		private void CloseView()
-		{
-			viewController.Close();
-			viewShown = false;
-			UpdateMenu();
-		}
-
-		private void ShowView()
-		{
-			viewController.Show(this);
-			viewShown = true;
-			UpdateMenu();
-		}
-
-		private void UpdateMenu()
-		{
-			viewMenu.Checked = viewShown;
-		}
-
-	    void IFormClosedListener.FormClosed()
-	    {
+        void IFormClosedListener.FormClosed()
+        {
             viewShown = false;
             UpdateMenu();
         }
-	}
+
+        public void ToggleViewShown()
+        {
+            if (viewShown)
+            {
+                CloseView();
+            }
+            else
+            {
+                ShowView();
+            }
+            UpdateMenu();
+        }
+
+        private void viewMenu_Click(object sender, EventArgs e)
+        {
+            ToggleViewShown();
+        }
+
+        private void CloseView()
+        {
+            viewController.Close();
+            viewShown = false;
+            UpdateMenu();
+        }
+
+        private void ShowView()
+        {
+            viewController.Show(this);
+            viewShown = true;
+            UpdateMenu();
+        }
+
+        private void UpdateMenu()
+        {
+            viewMenu.Checked = viewShown;
+        }
+    }
 }
